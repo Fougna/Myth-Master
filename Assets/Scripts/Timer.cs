d76d92d5818a1debebf43846a16298130f51826c
Sebastien Ferran <sebferran@gmail.com>
2022-06-17 14:10:10 +0200

    Updated timer script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,9 +10,15 @@ public class Timer : MonoBehaviour
     // Serialized variable of time left to show the correct answer when the player finished answering (right or wrong).
     [SerializeField] float timeToShowCorrectAnswer = 10f;
 
+    // Public boolean variable to load (or not) the next question.
+    public bool loadNextQuestion;
+
     // Boolean variable to indicate if the player is answering a question (playing the game).
     // The variable is public and set by default to false.
     public bool isAnsweringQuestion = false;
+
+    // Public variable to split the timer image depending on the time left.
+    public float fillFraction;
 
     // Time left on the timer variable
     float timerValue;
@@ -22,20 +28,33 @@ public class Timer : MonoBehaviour
         UpdateTimer();
     }
 
+    // Time stop method, used to stop the timer when the question is answered (right or wrong).
+    // This method is public so the script can communicate with the timer image.
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+
     // Timer Update Method
     void UpdateTimer()
     {
         // At this point, the timer value is empty.
-        // But when assigned a value, it will be decreased like a real timer with the Time.deltatime class.
+        // But when assigned a value, it will be decreasing like a real timer with the Time.deltatime class.
         timerValue -= Time.deltaTime;
 
-        // The timer value will be assigned a value only if the status of the player is active (answering or not).
+        // The timer value will be assigned a value only if the status of the player is answering the question.
         if (isAnsweringQuestion)
         {
-            // If the timer reaches 0...
-            if (timerValue <= 0)
+            // If the timer is superior to zero...
+            if (timerValue > 0)
             {
-                // And the player cannot answer the question...
+                // The timer image will be emptied with a timer value to complete the question divided by its same value.
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            // Otherwise...
+            else
+            {
+                // The player cannot answer the question...
                 isAnsweringQuestion = false;
                 // The timer will last the amount of time needed to show the correct answer (right or wrong).
                 timerValue = timeToShowCorrectAnswer;
@@ -44,16 +63,23 @@ public class Timer : MonoBehaviour
         // Otherwise...
         else
         {
-            // If the timer reaches 0...
-            if (timerValue <=0)
+            // If the timer is superior to zero...
+            if (timerValue > 0)
             {
-                // And the player can answer the question...
+                // The timer image will be emptied with a timer value to show the correct answer divided by its same value.
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            // Otherwise...
+            else
+            {
+                // The player can answer the question...
                 isAnsweringQuestion = true;
                 // The timer will last the amount of time needed for the player to answer the question.
                 timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
             }
         }
 
-        Debug.Log(timerValue);
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
     }
 }
